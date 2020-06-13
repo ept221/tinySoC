c43149907351162d7adfd32e067e8eea7e0da40d
ept221 <ept221@lehigh.edu>
2020-06-13 15:25:44 -0400

    optimized regFile: synth is smaller and faster

--- src/regFile.v
+++ src/regFile.v
@@ -17,22 +17,52 @@ module regFile(input wire [3:0] inSelect,
             rFile[i] = 8'd0;
         end
     end
-
+    
     always @(posedge clk) begin
         if(write_en) begin
             rFile[inSelect] <= in;
         end
-        if(inc && (~write_en || (write_en && ~(inSelect == outBselect)))) begin
-            {rFile[outBselect + 1],rFile[outBselect]} <= {rFile[outBselect + 1],rFile[outBselect]} + 1;
-
+        if(inc&& (~write_en || (write_en && ~(inSelect == outBselect)))) begin
+            case(outBselect)
+            4'b0000:    {rFile[1],rFile[0]} <= {rFile[1],rFile[0]} + 1;
+            4'b0010:    {rFile[3],rFile[2]} <= {rFile[3],rFile[2]} + 1;
+            4'b0100:    {rFile[5],rFile[4]} <= {rFile[5],rFile[4]} + 1;
+            4'b0110:    {rFile[7],rFile[6]} <= {rFile[7],rFile[6]} + 1;
+            4'b1000:    {rFile[9],rFile[8]} <= {rFile[9],rFile[8]} + 1;
+            4'b1010:    {rFile[11],rFile[10]} <= {rFile[11],rFile[10]} + 1;
+            4'b1100:    {rFile[13],rFile[12]} <= {rFile[13],rFile[12]} + 1;
+            4'b1110:    {rFile[15],rFile[14]} <= {rFile[15],rFile[14]} + 1;
+            endcase
         end
         else if(dec && (~write_en || (write_en && ~(inSelect == outBselect)))) begin
-            {rFile[outBselect + 1],rFile[outBselect]} <= {rFile[outBselect + 1],rFile[outBselect]} - 1;
+            case(outBselect)
+            4'b0000:    {rFile[1],rFile[0]} <= {rFile[1],rFile[0]} + 1;
+            4'b0010:    {rFile[3],rFile[2]} <= {rFile[3],rFile[2]} + 1;
+            4'b0100:    {rFile[5],rFile[4]} <= {rFile[5],rFile[4]} + 1;
+            4'b0110:    {rFile[7],rFile[6]} <= {rFile[7],rFile[6]} + 1;
+            4'b1000:    {rFile[9],rFile[8]} <= {rFile[9],rFile[8]} + 1;
+            4'b1010:    {rFile[11],rFile[10]} <= {rFile[11],rFile[10]} + 1;
+            4'b1100:    {rFile[13],rFile[12]} <= {rFile[13],rFile[12]} + 1;
+            4'b1110:    {rFile[15],rFile[14]} <= {rFile[15],rFile[14]} + 1;
+            endcase
         end
     end
 
     assign  outA = rFile[inSelect];
     assign  outB = rFile[outBselect];
-    assign  outC = rFile[outBselect + 1];
+
+    always @(*) begin
+        case(outBselect)
+        4'b0000:    outC = rFile[1];
+        4'b0010:    outC = rFile[3];
+        4'b0100:    outC = rFile[5];
+        4'b0110:    outC = rFile[7];
+        4'b1000:    outC = rFile[9];
+        4'b1010:    outC = rFile[11];
+        4'b1100:    outC = rFile[13];
+        4'b1110:    outC = rFile[15];
+        default     outC = 0;
+        endcase
+    end
 
 endmodule
