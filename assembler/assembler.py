9ca5c5fff160a085df07ef6ae7b2fbf750707955
ept221 <ept221@lehigh.edu>
2020-07-06 22:03:25 -0400

    starting assembler

---
+++ assembler/assembler.py
@@ -0,0 +1,59 @@
+##############################################################################################################
+import re
+import sys
+
+##############################################################################################################
+# File reading functions
+
+def read(name):
+    # This function reads in lines from the asm file
+    # It processes them and puts them into the form:
+    # [[Line_number, Program_Counter] [body] [comment]]
+    # Line_number corrisponds to the line on the 
+    # source code. the Program_Counter is incremented
+    # every time there is a non-empty line. Note that
+    # two consecutive PC locations do NOT nessisarily
+    # corrispond to two consecutive address locations
+
+    # [[Line_number, Program_Counter] [body] 'comment']
+    
+    file = open(name, 'r')
+    lines = []
+    lineNumber = 0
+    pc = 0
+    
+    for lineNumber, line in enumerate(file, start = 1):
+        line = line.strip()
+        line = line.upper()
+        if(line):
+            block = []
+            rest = [] 													# The input line without the comment
+            comment = ''
+            commentIndex = line.find(";")
+            if(commentIndex != -1):
+                comment = line[commentIndex:]
+                rest = line[:commentIndex].strip()
+            else:
+                rest = line
+
+            block.append([lineNumber, pc])
+            if(rest): 												   # If we have code after we strip any comment out
+                split_rest = re.split(r'([-+,\s]\s*)', rest)
+                split_rest = [word for word in split_rest if not re.match(r'^\s*$',word)]
+                split_rest = list(filter(None, split_rest))
+                block.append(split_rest)
+            else:
+                block.append([])
+            block.append(comment)
+            lines.append(block)
+            pc += 1
+            
+    file.close()
+    return lines
+##############################################################################################################
+# 
+
+lines = read("programs/demo.asm");
+
+for line in lines:
+	print(line)
---
+++ programs/demo.asm
@@ -0,0 +1,25 @@
+        ldi r0, 1               ; set pin 1 to output
+        out r0, 0 
+
+        ldi r14, ff             ; setup the stack pointer
+
+
+        ldi r0, 00011110        ; setup the gpu control register
+        out r0, 10000000
+
+        ldi r2, 0x00            ; setup the vram pointer
+        ldi r3, 0x20
+
+        ssr 8                   ; enable interrupts
+loop:   jmp loop                ; do nothing and wait for an interrupt
+
+
+
+
+isr:    in r0, 2                ; read pin 1
+        xoi r0, 1               ; flip the bit
+        out r0 1                ; toggle pin 1
+        ldi r0, 65
+        str r0, p1
+        ssr 8                   ; enable interrupts
+        ret                     ; return
