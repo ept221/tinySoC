27b6684629c2e7d11df995d5e2322a38065f347f
ept221 <ept221@lehigh.edu>
2020-07-22 12:55:01 -0400

    realized I need a mov instruction

--- programs/general_test.asm
+++ programs/general_test.asm
@@ -2,8 +2,14 @@
         .define dir_reg, 0x00
         .define port_reg, 0x01
         .define pin_reg, 0x02
+
+        .define prescaler_l, 0x03
+        .define prescaler_h, 0x04
+        .define count_ctrl, 0x05
+
         .define gpu_addr, 0x2000
         .define gpu_ctrl_reg, 0x80
+
         .define gpu_isr_vector, 0x0020
         .define top_isr_vector, 0x0030
 ;******************************************************************************
@@ -17,28 +23,19 @@
 
         ldi r14, 0xff           ; set stack pointer
 
-        ldi r2, 36
-        out r2, 3               ; set LSBs of prescaler
-
-        ldi r2, 244
-        out r2, 4               ; set MSPs of prescaler
-
-        ldi r2, 18          
-        out r2, 5               ; set pwm mode, set top interrupt
+        ldi r2, 0b00011110      ; configure gpu
+        out r2, gpu_ctrl_reg
 
         ssr 8                   ; enable interrupts
+loop:   jmp loop
 
-poll:   in r2, pin_reg          ; read pin register
-        ani r2, 2               ; mask the button
-        srl r2                  ; get the state of the button in the lsb
-        adi r2, 48              ; calculate the ascii (0 or 1) state of the button
-        str r2, p0              ; write the char to the screen
-        jmp poll                ; poll the button again
-
-        .org top_isr_vector
-isr:    in r3, 2                ; read the status of the led
-        xoi r3, 1               ; flip the first bit
-        out r3, 1               ; toggle the led
+        .org gpu_isr_vector
+isr:    in r3, pin_reg          ; read pin register
+        ani r3, 2               ; mask the button
+        srl r3                  ; get the state of the button in the lsb
+        out r3, port_reg        ; write the state to the led
+        adi r3, 48              ; calculate the ascii (0 or 1) state of the button
+        str r3, p0              ; write the char to the screen
         ssr 8                   ; enable interrupts
         ret                     ; return from isr
 ;******************************************************************************
