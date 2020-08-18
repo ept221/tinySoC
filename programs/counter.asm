a27645ba4023a5b9c39a796b4af137b28ebf139d
ept221 <ept221@lehigh.edu>
2020-08-18 11:52:38 -0400

    updated formatting

--- programs/counter.asm
+++ programs/counter.asm
@@ -12,46 +12,46 @@
 
         .define gpu_isr_vector, 0x0020
         .define top_isr_vector, 0x0030
-;******************************************************************************		
-		.code
-		
-		ldi r14, 0xff			; set stack pointer
+;******************************************************************************         
+        .code
+                
+        ldi r14, 0xff                   ; set stack pointer
 
-		ldi r0, 0b00011000
+        ldi r0, 0b00011000
         out r0, gpu_ctrl_reg
 
         ldi r2, gpu_addr[l]
         ldi r3, gpu_addr[h]
 
-		ldi r0, 1
-		out r0, dir_reg			; set pin 1 to output
+        ldi r0, 1
+        out r0, dir_reg                 ; set pin 1 to output
 
-		ldi r0, 36
-		out r0, prescaler_l 	; set LSBs of prescaler
+        ldi r0, 36
+        out r0, prescaler_l             ; set LSBs of prescaler
 
-		ldi r0, 244
-		out r0, prescaler_h 	; set MSPs of prescaler
+        ldi r0, 244
+        out r0, prescaler_h             ; set MSPs of prescaler
 
-		ldi r0, 0b00010010
-		out r0, count_ctrl		; set pwm mode, set top interrupt
+        ldi r0, 0b00010010
+        out r0, count_ctrl              ; set pwm mode, set top interrupt
 
-		ldi r5, 0
+        ldi r5, 0
 
-		ssr 8 					; enable interrupts
-loop:	jmp loop				; loop and wait for interrupt
+        ssr 8                           ; enable interrupts
+loop:   jmp loop                        ; loop and wait for interrupt
 ;******************************************************************************
-		.org top_isr_vector
-isr:    in r0, port_reg			; read pin register
-		xoi r0, 1				; toggle the led bit
-		out r0, port_reg 		; write to the port register
-		jz exit
-		adi r5, 1
-		mov r12, r5
-		call numToStr
-		ldi r2, gpu_addr[l]
+        .org top_isr_vector
+isr:    in r0, port_reg                 ; read pin register
+        xoi r0, 1                       ; toggle the led bit
+        out r0, port_reg                ; write to the port register
+        jz exit
+        adi r5, 1
+        mov r12, r5
+        call numToStr
+        ldi r2, gpu_addr[l]
         ldi r3, gpu_addr[h]
-exit:   ssr 8					; enable interrupts
-		ret
+exit:   ssr 8                           ; enable interrupts
+        ret
 ;******************************************************************************
 numToStr:
         mov r13, r12
