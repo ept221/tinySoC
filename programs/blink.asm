12ff6c7b45d4860ec0139888a8e111975bd7086d
ept221 <ept221@lehigh.edu>
2020-08-21 15:45:50 -0400

    updated blink.asm

--- programs/blink.asm
+++ programs/blink.asm
@@ -12,11 +12,30 @@
 
         .define gpu_isr_vector, 0x0020
         .define top_isr_vector, 0x0030
-;******************************************************************************		
-		.code
-		
-		ldi r0, 0x01
-		out r0, dir_reg
-		out r0, port_reg
-		hlt
+;******************************************************************************         
+        .code
+                
+        ldi r14, 0xff                   ; set stack pointer
+
+        ldi r0, 1
+        out r0, dir_reg                 ; set pin 1 to output
+
+        ldi r0, 36
+        out r0, prescaler_l             ; set LSBs of prescaler
+
+        ldi r0, 244
+        out r0, prescaler_h             ; set MSPs of prescaler
+
+        ldi r0, 0b00010010
+        out r0, count_ctrl              ; set pwm mode, set top interrupt
+
+        ssr 8                           ; enable interrupts
+loop:   jmp loop                        ; loop and wait for interrupt
+
+        .org top_isr_vector
+isr:    in r0, port_reg                 ; read pin register
+        xoi r0, 1                       ; toggle the led bit
+        out r0, port_reg                ; write to the port register
+        ssr 8                           ; enable interrupts
+        ret
 ;******************************************************************************
